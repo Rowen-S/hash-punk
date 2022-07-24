fc16ce40557b8e2ccb2203e20dbd4f9643bec414
Rowen <15138737639@163.com>
2022-07-24 18:32:47 +0800

    update constants address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -16,5 +16,5 @@ export const ENS_REGISTRAR_ADDRESSES: AddressMap = {
 
 export const MINT_ADDRESS: AddressMap = {
   ...constructSameAddressMap('0x37205a4Be031dC402066E39D181846c026e66eD7'),
-  [SupportedChainId.GOERLI]: '0x8A906aE92C96c006056F2257a0609fCDF5C65C38',
+  [SupportedChainId.GOERLI]: '0xF67E240E1663c67C9896328A779516d0d78EC37E',
 }
